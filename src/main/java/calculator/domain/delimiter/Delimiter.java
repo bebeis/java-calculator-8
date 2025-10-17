cc5cae3e1b379e3156b25a00dc868c61aa9ee1e0
Junseo Bae <ferrater1013@gmail.com>
2025-10-17 19:12:47 +0900

    feat(delimiter): 구분자 값 검증 로직 추가
    - 구분자가 모두 숫자로 구성된 경우 예외 발생
    - 구분자가 비어 있는 경우 예외 발생
    - 제어 문자(탭 제외)가 포함된 경우 예외 발생
    - 정규식 문자로 변환 가능

---
+++ src/main/java/calculator/domain/delimiter/Delimiter.java
@@ -0,0 +1,44 @@
+package calculator.domain.delimiter;
+
+import java.util.regex.Pattern;
+
+public class Delimiter {
+    private final String value;
+
+    public Delimiter(final String value) {
+        validatePossibleValue(value);
+        this.value = value;
+    }
+
+    private void validatePossibleValue(final String value) {
+        validateNotAllDigits(value);
+        validateNotEmpty(value);
+        validateNoControlCharacterExcludeTab(value);
+    }
+
+    private void validateNotEmpty(final String value) {
+        if (value.isEmpty()) {
+            throw new IllegalArgumentException("구분자는 공백일 수 없습니다");
+        }
+    }
+
+    private void validateNotAllDigits(final String value) {
+        // TODO: 정규식으로 개선하기
+        if (value.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("구분자가 모두 숫자로 구성될 수 없습니다.");
+        }
+    }
+
+    private void validateNoControlCharacterExcludeTab(final String value) {
+        // TODO: 정규식으로 개선하기
+        for (char c : value.toCharArray()) {
+            if (Character.isISOControl(c) && c != '\t') {
+                throw new IllegalArgumentException("문자열에 제어문자가 포함되어 있습니다.");
+            }
+        }
+    }
+
+    public String toRegexToken() {
+        return Pattern.quote(value);
+    }
+}
