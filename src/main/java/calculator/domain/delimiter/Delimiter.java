9edcceea75b16185ee0f57701c86efceefa5e9bd
Junseo Bae <ferrater1013@gmail.com>
2025-10-17 19:48:16 +0900

    feat(delimiter): 구분자 최대 길이에 따른 검증 기능 추가
    - 구분자가 최대 길이를 넘을 시 예외가 발생한다.

--- src/main/java/calculator/domain/delimiter/Delimiter.java
+++ src/main/java/calculator/domain/delimiter/Delimiter.java
@@ -3,6 +3,8 @@ package calculator.domain.delimiter;
 import java.util.regex.Pattern;
 
 public class Delimiter {
+    private static final int MAX_LENGTH = 30;
+
     private final String value;
 
     public Delimiter(final String value) {
@@ -14,6 +16,7 @@ public class Delimiter {
         validateNotAllDigits(value);
         validateNotEmpty(value);
         validateNoControlCharacterExcludeTab(value);
+        validateDelimiterLength(value);
     }
 
     private void validateNotEmpty(final String value) {
@@ -35,6 +38,12 @@ public class Delimiter {
             if (Character.isISOControl(c) && c != '\t') {
                 throw new IllegalArgumentException("문자열에 제어문자가 포함되어 있습니다.");
             }
+        }
+    }
+
+    private void validateDelimiterLength(final String value) {
+        if (value.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException("구분자가 너무 깁니다. 30자 이내로 작성해주세요");
         }
     }
 
