439f0bf2a0425711ceebeb0884c03daa2a233c40
Junseo Bae <ferrater1013@gmail.com>
2025-10-17 09:52:27 +0900

    feat: 콘솔 출력 처리 구현
    - 초기 온보딩 메시지를 출력합니다.
    - 덧셈 결과를 출력합니다.

---
+++ src/main/java/calculator/io/ConsoleOutputHandler.java
@@ -0,0 +1,14 @@
+package calculator.io;
+
+import calculator.domain.PositiveNumber;
+
+public class ConsoleOutputHandler {
+
+    public void printInitMessage() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public void printResult(final PositiveNumber number) {
+        System.out.println("결과 : " + number);
+    }
+}
