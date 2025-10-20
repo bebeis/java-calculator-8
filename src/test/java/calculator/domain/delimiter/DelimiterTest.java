99c69e45e6ab115d94200933dceed3d94cf6db6f
Junseo Bae <ferrater1013@gmail.com>
2025-10-20 13:30:15 +0900

    test(delimiter): 테스트 코드 검증 개선
    - 예외 타입을 IllegalArgumentException으로 구체화

--- src/test/java/calculator/domain/delimiter/DelimiterTest.java
+++ src/test/java/calculator/domain/delimiter/DelimiterTest.java
@@ -20,9 +20,9 @@ class DelimiterTest {
     @DisplayName("구분자는 숫자만으로 구성될 수 없다.")
     @Test
     void numericOnlyDelimiter_throwsException() {
-        assertThatThrownBy(() -> new Delimiter("12"));
-        assertThatThrownBy(() -> new Delimiter("11"));
-        assertThatThrownBy(() -> new Delimiter("1"));
+        assertThatThrownBy(() -> new Delimiter("12")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Delimiter("11")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Delimiter("1")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구분자는 공백문자를 포함할 수 있다.")
@@ -38,7 +38,7 @@ class DelimiterTest {
     @DisplayName("구분자는 빈 문자열일 수 없다.")
     @Test
     void emptyString_throwsException() {
-        assertThatThrownBy(() -> new Delimiter(""));
+        assertThatThrownBy(() -> new Delimiter("")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구분자는 tab을 포함할 수 있다.")
@@ -53,16 +53,16 @@ class DelimiterTest {
     @DisplayName("구분자는 탭을 제외한 제어문자를 포함할 수 없다.")
     @Test
     void containsControlCharacter_throwsException() {
-        assertThatThrownBy(() -> new Delimiter("\u0000a"));
-        assertThatThrownBy(() -> new Delimiter("c\u0004b"));
-        assertThatThrownBy(() -> new Delimiter("a\nd"));
-        assertThatThrownBy(() -> new Delimiter("z\n"));
+        assertThatThrownBy(() -> new Delimiter("\u0000a")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Delimiter("c\u0004b")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Delimiter("a\nd")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Delimiter("z\n")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구분자는 최대 길이를 초과할 수 없다.")
     @Test
     void exceedMaxLength_throwsException() {
-        assertThatThrownBy(() -> new Delimiter("abcdefghijkalmfoisdfkljaszxgsfasfwe"));
+        assertThatThrownBy(() -> new Delimiter("abcdefghijkalmfoisdfkljaszxgsfasfwe")).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("정규식 특수문자는 이스케이프처리한다")
