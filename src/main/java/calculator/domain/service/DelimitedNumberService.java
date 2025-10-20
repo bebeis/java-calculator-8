ad4573b0d794737d5a9f2f59d7918333798dfab9
Junseo Bae <ferrater1013@gmail.com>
2025-10-20 14:58:50 +0900

    feat: DelimitedNumberService 클래스 추가 및 숫자 추출 기능 구현 - 문자열을 받고 숫자를 추출

---
+++ src/main/java/calculator/domain/service/DelimitedNumberService.java
@@ -0,0 +1,27 @@
+package calculator.domain.service;
+
+import calculator.domain.delimiter.Delimiter;
+import calculator.domain.delimiter.DelimiterGroup;
+import calculator.domain.number.PositiveNumber;
+import calculator.domain.service.extractor.CustomDelimiterExtractor;
+import calculator.domain.service.extractor.NumberExtractor;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DelimitedNumberService {
+
+    private final CustomDelimiterExtractor delimiterExtractor = new CustomDelimiterExtractor();
+    private final NumberExtractor numberExtractor = new NumberExtractor();
+
+    public List<PositiveNumber> extractNumbersFrom(String delimitedNumberExpr) {
+        Optional<Delimiter> customDelimiter = delimiterExtractor.extractDelimitersFrom(delimitedNumberExpr);
+
+        DelimiterGroup delimiterGroup = customDelimiter
+                .map(DelimiterGroup.defaultDelimiterGroup()::addCustomDelimiter)
+                .orElse(DelimiterGroup.defaultDelimiterGroup());
+
+        String regex = delimiterGroup.toRegexForSplit();
+        return numberExtractor.extractNumbers(delimitedNumberExpr, regex);
+    }
+}
