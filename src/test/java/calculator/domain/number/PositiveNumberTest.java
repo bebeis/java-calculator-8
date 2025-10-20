cc0c4af54282dee405beadb001e2c735c1cddb90
Junseo Bae <ferrater1013@gmail.com>
2025-10-20 13:34:37 +0900

    chore: 테스트 코드 패키지 변경

--- src/test/java/calculator/domain/number/PositiveNumberTest.java
+++ src/test/java/calculator/domain/number/PositiveNumberTest.java
@@ -1,6 +1,5 @@
-package calculator.domain;
+package calculator.domain.number;
 
-import calculator.domain.number.PositiveNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
