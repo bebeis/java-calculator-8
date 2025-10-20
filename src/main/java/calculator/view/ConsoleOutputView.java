1731933f389ca0bbcd6a7c0b3110dddb796f3338
Junseo Bae <ferrater1013@gmail.com>
2025-10-20 12:18:07 +0900

    refactor: outputView 파라미터 수정
    - View에서 도메인에 의존하지 않도록 수정

--- src/main/java/calculator/view/ConsoleOutputView.java
+++ src/main/java/calculator/view/ConsoleOutputView.java
@@ -1,14 +1,12 @@
 package calculator.view;
 
-import calculator.domain.PositiveNumber;
-
 public class ConsoleOutputView {
 
     public void printInitMessage() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
     }
 
-    public void printResult(final PositiveNumber number) {
-        System.out.println("결과 : " + number);
+    public void printResult(final String result) {
+        System.out.println("결과 : " + result);
     }
 }
