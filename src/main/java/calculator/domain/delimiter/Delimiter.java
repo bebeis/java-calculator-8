5ca5d363686d65cc0f35a40dce77cb8726041805
Junseo Bae <ferrater1013@gmail.com>
2025-10-20 12:16:15 +0900

    refactor(delimiter): 검증 처리 로직 개선
    - 정규식을 활용하여 구분자 검증

--- src/main/java/calculator/domain/delimiter/Delimiter.java
+++ src/main/java/calculator/domain/delimiter/Delimiter.java
@@ -1,5 +1,6 @@
 package calculator.domain.delimiter;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class Delimiter {
@@ -26,18 +27,21 @@ public class Delimiter {
     }
 
     private void validateNotAllDigits(final String value) {
-        // TODO: 정규식으로 개선하기
-        if (value.chars().allMatch(Character::isDigit)) {
+        if (value.matches("^\\d+$")) {
             throw new IllegalArgumentException("구분자가 모두 숫자로 구성될 수 없습니다.");
         }
     }
 
+
     private void validateNoControlCharacterExcludeTab(final String value) {
-        // TODO: 정규식으로 개선하기
         for (char c : value.toCharArray()) {
-            if (Character.isISOControl(c) && c != '\t') {
-                throw new IllegalArgumentException("문자열에 제어문자가 포함되어 있습니다.");
-            }
+            validateNoControlCharacterExceptTab(c);
+        }
+    }
+
+    private void validateNoControlCharacterExceptTab(final char c) {
+        if (Character.isISOControl(c) && c != '\t') {
+            throw new IllegalArgumentException("문자열에 제어문자가 포함되어 있습니다.");
         }
     }
 
@@ -49,5 +53,17 @@ public class Delimiter {
 
     public String toRegexToken() {
         return Pattern.quote(value);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Delimiter delimiter = (Delimiter) o;
+        return Objects.equals(value, delimiter.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
     }
 }
