276e45762585785c02551cae80661afa58656def
Junseo Bae <ferrater1013@gmail.com>
2025-10-20 14:57:40 +0900

    feat: CalculatorService 클래스 추가 및 계산 기능 구현 - 도메인 기능을 이용해 덧셈한 결과를 반환

---
+++ src/main/java/calculator/service/CalculatorService.java
@@ -0,0 +1,24 @@
+package calculator.service;
+
+import calculator.domain.number.PositiveNumber;
+import calculator.domain.service.DelimitedNumberService;
+import calculator.domain.service.adder.Adder;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CalculatorService {
+
+    private final DelimitedNumberService resolver = new DelimitedNumberService();
+    private final Adder adder = new Adder();
+
+    public String calculate(final String calculationRequest) {
+        // "숫자를 추출"하는 것과 "더하는" 것은 별도의 유즈케이스로 해석함.
+        // 추후, 뺄샘이 생기면 subtracter 이런 것만 추가해줘도 됨
+        List<PositiveNumber> positiveNumbers = resolver.extractNumbersFrom(calculationRequest);
+        Optional<PositiveNumber> sumOpt = adder.sum(positiveNumbers);
+
+        // 더할 숫자가 없는 경우는 0을 반환한다는 Application 규칙으로 판단함. 도메인 규칙이 아님!
+        return sumOpt.map(PositiveNumber::toString).orElse("0");
+    }
+}
